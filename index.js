efa0aa0f738ed4a942b86ff1327be97391733dae
Aman Parida <amantechz467@gmail.com>
2025-05-25 16:29:39 +0530

    Use MongoDB Atlas URI from environment variable

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ main()
     .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
+    await mongoose.connect(process.env.MONGO_URI);
 }
 
 // Index Route
